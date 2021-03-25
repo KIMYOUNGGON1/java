61b1a1b64018ca4ea623b3a61950284c7c6f806f
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-03-25 16:36:30 +0900

    수정사항

--- src/p02/lecture/A07TypeCharEx1.java
+++ src/p02/lecture/A07TypeCharEx1.java
@@ -8,16 +8,16 @@ public class A07TypeCharEx1 {
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
 
-//			if (( 0x61 <= c && c <= 0x7a) || (0x41 <= c && c < 0x5a)) {
+//			if (( 0x61 <= c && c <= 0x7a) || (0x41 <= c && c <= 0x5a)) {
 //				count++;
 //			} 
 //			16진법
 			
-//			if ((97 <= c && c <= 122) || (65 <= c && c < 90)) {
+//			if ((97 <= c && c <= 122) || (65 <= c && c <= 90)) {
 //				count++;
 //			}
 			
-			if (('a'<= c && c <= 'z') || ('A' <= c && c < 'Z')) {
+			if (('a'<= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
 				count++;
 			}
 		}
