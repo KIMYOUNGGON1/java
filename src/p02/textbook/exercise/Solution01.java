46960eaad956ee35c3c045e5539fa4f928936352
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-03-26 12:07:46 +0900

    20210326 3교시 수업내용

---
+++ src/p02/textbook/exercise/Solution01.java
@@ -0,0 +1,47 @@
+package p02.textbook.exercise;
+
+public class Solution01 {
+	public static void main(String[] args) {
+//		1번 문제 : 4
+		int v;
+		
+//		System.out.println(v);
+		
+//		2번 문제;
+		int modelName;
+//		int class;
+//		int 6hour;
+		int $value;
+		int _age;
+//		int int
+		
+//		3번 문제
+//		
+//		1byte	2byte	  4byte	   8byte
+//		정수
+//		byte	short,char int		long
+//		실수
+//						  float		double
+//		논리
+//		boolean
+//		
+//		String 문자열 ""
+		
+//		4번 문제
+		int age;
+		age = 10;
+		double price = 3.14;
+		
+//		타입: int, double
+//		변수명: age, price
+//		리터럴: 10, 3.14
+		
+//		7번 문제
+		
+		int var1 = 10;
+		long var2 = 10000000L;
+//		char var3 = '';
+		double var4 = 10;
+		float var5 = 10;
+	}
+}
