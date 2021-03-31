b4f6cc810f560bf6bd833676ac0f145b3429ac06
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-03-31 09:40:25 +0900

    20210331 1교시 수업내용

--- src/p04/textbook/IfNestedExample.java
+++ src/p04/textbook/IfNestedExample.java
@@ -2,7 +2,7 @@ package p04.textbook;
 
 public class IfNestedExample {
 	public static void main(String[] args) {
-		int score = (int) (Math.random() * 20) + 81;
+		int score = (int) (Math.random() * 20) + 81; //81이상 100이하
 		System.out.println("점수: " + score);
 
 		String grade;
