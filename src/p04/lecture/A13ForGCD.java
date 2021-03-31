f9cdfe4239c30b3f948f3a898d99caeec78c226b
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-03-31 16:30:16 +0900

    20210331 7교시 수업내용

--- src/p04/lecture/A13ForGCD.java
+++ src/p04/lecture/A13ForGCD.java
@@ -33,6 +33,47 @@ public class A13ForGCD {
 //		}
 //		return a;
 		
+		System.out.println("------------------------------");
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("두 수를 입력하세요.>");
+//		
+//		int num1 = scanner.nextInt();
+//		int num2 = scanner.nextInt();
+//		
+//		System.out.println("입력 받은 두 수는 " + num1 +"과" + num2);
+//		
+//		for (int d = num1; d >= 1; d--) {
+//			if((num1%d)==0 && (num2 % d)==0) {
+//				System.out.println("최대공약수는 " + d);
+//				return;
+//			}
+//		}
+//		
+//		System.out.println("프로그램을 종료합니다.");
+//		scanner.close();
+		
+		System.out.println("---------------");
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("두 수를 입력하세요.>");
+//		
+//		int num1 = scanner.nextInt();
+//		int num2 = scanner.nextInt();
+//		
+//		System.out.println("입력 받은 두 수는 " + num1 +"과" + num2);
+//		
+//		for (int d = num1; d >= 1; d--) {
+//			if((num1%d)==0 || (num2 % d)==0) {
+//				System.out.println("최소공배수는 " + d);
+//				return;
+//			}
+//		}
+//		
+//		scanner.close();
+	
+		System.out.println("---------------");
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("두 수를 입력하세요.>");
 		
@@ -44,8 +85,12 @@ public class A13ForGCD {
 		for (int d = num1; d >= 1; d--) {
 			if((num1%d)==0 && (num2 % d)==0) {
 				System.out.println("최대공약수는 " + d);
-				return;
+				break;
 			}
 		}
+		
+		System.out.println("프로그램을 종료합니다.");
+		scanner.close();
+		
 	}
 }
