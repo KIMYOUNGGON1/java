0f8ccf67207ac8e31982b81a877ce49af2e4359b
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-01 15:41:22 +0900

    20210401 6교시 수업내용 빼먹은 것.

--- src/P05/lecture/A01ReferenceType.java
+++ src/P05/lecture/A01ReferenceType.java
@@ -1,5 +1,7 @@
 package P05.lecture;
 
+import java.util.Scanner;
+
 public class A01ReferenceType {
 	public static void main(String[] args) {
 //		자료형 (2가지)
@@ -7,5 +9,22 @@ public class A01ReferenceType {
 //		    byte ,short, char, int, long, double, boolean, float
 //		2. 참조타입 reference type
 //		 	기본타입 외 모두
+		
+//		차이점
+//		기본타입은 데이터를 저장
+//		참조타입은 데이터를 저장한 객체의 참조를 저장
+		
+//		예제 (기본타입)
+		int i;
+//		4byte의 저장 공간 생성됨.
+		i = 3;
+//		i에 3이 들어감.
+
+		
+//		예제 (참조타입)
+		Scanner scanner;
+//		scanner라는 저장 공간 생성됨.
+		scanner = new Scanner(System.in);
+		
 	}
 }
