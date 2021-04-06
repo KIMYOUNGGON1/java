54857dc424a118b1f5b16698e134235ac039564d
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-06 11:41:10 +0900

    20210406 3교시 수업내용

---
+++ src/P06/lecture/p1class/Student.java
@@ -0,0 +1,17 @@
+package P06.lecture.p1class;
+
+// 	객체를 만들기 위한 설계도 (class)
+
+//	class 명 : UpperCamelCase
+//	숫자, 영문, $, _ 조합 : 숫자가 앞에 오면 안됨.
+//	관습상 class명의 첫 글자는 대문자로 작성
+
+//	class 키워드가 앞에 옴
+
+//	class 명 다음에 class 본문({})중괄호로 작성
+
+public class Student {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/P06/textbook/s060401/Student.java
@@ -0,0 +1,5 @@
+package P06.textbook.s060401;
+
+public class Student {
+
+}
