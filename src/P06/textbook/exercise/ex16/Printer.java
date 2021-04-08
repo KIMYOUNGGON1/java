84ea728377989a6a833d98138540b34e343264bc
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-08 11:40:50 +0900

    20210408 3교시 수업내용

---
+++ src/P06/textbook/exercise/ex16/Printer.java
@@ -0,0 +1,19 @@
+package P06.textbook.exercise.ex16;
+
+public class Printer {
+	public void println (int a) {
+		System.out.println(a);
+	}
+	
+	public void println (boolean a) {
+		System.out.println(a);
+	}
+
+	public void println (double a) {
+		System.out.println(a);
+	}
+	
+	public void println (String a) {
+		System.out.println(a);
+	}
+}
