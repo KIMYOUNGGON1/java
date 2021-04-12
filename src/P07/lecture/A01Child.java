e46e2cb2ca69708ffa15c3b171c0a8183c22fc04
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-12 09:41:45 +0900

    20210412 1교시 수업내용

---
+++ src/P07/lecture/A01Child.java
@@ -0,0 +1,5 @@
+package P07.lecture;
+
+public class A01Child extends A01parent {
+
+}
---
+++ src/P07/lecture/A01parent.java
@@ -0,0 +1,18 @@
+package P07.lecture;
+
+public class A01parent {
+	
+	public int i;
+	
+	public A01parent() {
+		
+	}
+	
+	public A01parent(int i) {
+		this.i=i;
+	}
+	
+	public void method1() {
+		System.out.println("method1");
+	}
+}
