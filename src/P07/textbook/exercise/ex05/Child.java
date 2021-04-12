263347cad2ab3a63b284a12ada206a2f846a756f
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-12 12:00:54 +0900

    20210412 3교시 수업내용

---
+++ src/P07/textbook/exercise/ex05/Child.java
@@ -0,0 +1,10 @@
+package P07.textbook.exercise.ex05;
+
+public class Child extends Parent{
+	private int studentNo;
+	
+	public Child(String name, int studentNo) {
+		Super.parent(name);
+		this.studentNo = studentNo;
+//	}
+}
---
+++ src/P07/textbook/exercise/ex06/Parent.java
@@ -0,0 +1,12 @@
+package P07.textbook.exercise.ex06;
+
+public class Parent {
+	public String nation;
+	
+	public Parent() {
+		this("대한민국");
+		System.out.println("Parent() call");
+		
+		public Parent
+	}
+}
