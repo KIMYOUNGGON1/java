898a683933cb23c59c5cff17f47dd7fa857a0a1a
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-14 09:57:42 +0900

    20210414 1교시 수업내용

---
+++ src/p08/lecture/ex2/A01Interface.java
@@ -0,0 +1,8 @@
+package p08.lecture.ex2;
+
+public class A01Interface {
+	public static void main(String[] args) {
+		System.out.println(MyInterface.MY_FIELD);
+		System.out.println(MyInterface.MY_FIELD2);
+	}
+}
---
+++ src/p08/lecture/ex2/MyInterface.java
@@ -0,0 +1,17 @@
+package p08.lecture.ex2;
+
+public interface MyInterface {
+	// public static final 필드
+	public static final int MY_FIELD = 1;
+	int MY_FIELD2 = 2;
+	// 선언 시 값을 배정해주면 됨. (public static fianl 생략 가능)
+	
+	
+	// public abstract 메소드 (인스턴스 메소드)
+	
+	// public static 메소드 (클래스 메소드)
+
+	// public default 메소드 (인스턴스 메소드)
+	
+	//생성자는 가지지 못함.
+}
