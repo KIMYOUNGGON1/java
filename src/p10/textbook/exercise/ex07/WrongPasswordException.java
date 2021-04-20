20acb675bc5486d5845c771dbdb924f535376229
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-20 12:03:30 +0900

    20210420 3교시 수정사항

--- src/p10/textbook/exercise/ex07/WrongPasswordException.java
+++ src/p10/textbook/exercise/ex07/WrongPasswordException.java
@@ -1,6 +1,6 @@
 package p10.textbook.exercise.ex07;
 
-public class WrongPasswordException {
+public class WrongPasswordException extends Exception{
 	public WrongPasswordException() {}
 	public WrongPasswordException(String message) {
 		super(message);
