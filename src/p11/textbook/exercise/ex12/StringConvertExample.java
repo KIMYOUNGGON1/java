ad1653fb5f6901ff5761163c9703c4ccb62c225e
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-22 17:27:16 +0900

    20210422 7교시 수업내용

---
+++ src/p11/textbook/exercise/ex12/StringConvertExample.java
@@ -0,0 +1,11 @@
+package p11.textbook.exercise.ex12;
+
+public class StringConvertExample {
+	public static void main(String[] args) {
+		String strData1 = "200";
+		int intData1 = Integer.parseInt(strData1);
+		
+		int intDate2 = 150;
+		String strDate2 = String.valueOf(intDate2);
+	}
+}
