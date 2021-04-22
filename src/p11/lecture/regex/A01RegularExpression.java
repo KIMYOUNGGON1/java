e9e10ac7338d971831bcbe531b4f262a5915f36b
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-22 12:41:39 +0900

    20210422 4교시 수업내용

---
+++ src/p11/lecture/regex/A01RegularExpression.java
@@ -0,0 +1,192 @@
+package p11.lecture.regex;
+
+import java.util.regex.Pattern;
+
+public class A01RegularExpression {
+	//정규표현식
+	//문자열의 패턴을 표현하는 문자열
+	//meta-character, quantifier
+	
+	public static void main(String[] args) {
+		String regex1 = "a";
+		String str1 = "a";
+		
+		boolean p1 = Pattern.matches("", "");
+		System.out.println(p1);
+		
+		regex1 = "java";
+		str1 = "java";
+		
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "9";
+		str1 = "0";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 ="\\d";
+		str1="8";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d";
+		str1="89";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d\\d";
+		str1 ="67";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d{3}";
+		str1 = "943";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d{3,}";
+		str1 = "98765123";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "010-\\d{4}-\\d{4}";
+		str1 = "010-9876-3333";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d{3,5}";
+		str1 = "224566";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d+";
+		str1 = "";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d*";
+		str1="0";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d?";
+		str1 = "";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "010-?\\d{4}-?\\d{4}";
+		str1 = "010-9876-3333";
+		String str2 = "01098763333";
+		String str3 = "010-98763333";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		
+		regex1 = "\\w";
+		str1 = "S";
+		str2 = "T";
+		str3 = "8";
+		String str4 = "_";
+		String str5 = " ";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		System.out.println(Pattern.matches(regex1, str5));
+		
+		regex1 = "\\w\\d+";
+		str1="e33526";
+		str2="223423462456";
+		str3="_12435";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		
+		regex1 = "(java)";
+		str1 = "java";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "(java)+";
+		str1 = "javajavajavaj";
+		str2 = "javajavajava";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		
+		regex1 = "(java|python)";
+		str1 = "java";
+		str2 = "python";
+		str3 = "spring";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		
+		regex1 = "(java|python)+";
+		str1 = "java";
+		str2 = "python";
+		str3 = "pythonjavajavapython";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		
+		regex1 = ".";
+		str1 = "$";
+		str2 = ".";
+		str3 = "A";
+		str4 = "9";
+		str5 = "_";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		System.out.println(Pattern.matches(regex1, str5));
+		
+		regex1 = "\\.";
+		str1 = "$";
+		str2 = ".";
+		str3 = "A";
+		str4 = "9";
+		str5 = "_";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		System.out.println(Pattern.matches(regex1, str5));
+		
+		regex1 = "[abc]";
+		str1 = "a";
+		str2 = "b";
+		str3 = "c";
+		str4 = "d";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		
+		regex1 = "[^abc]";
+		str1 = "a";
+		str2 = "b";
+		str3 = "c";
+		str4 = "d";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		
+		regex1 = "[a-z]";
+		str1 = "a";
+		str2 = "b";
+		str3 = "c";
+		str4 = "d";
+		str5 = "A";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		System.out.println(Pattern.matches(regex1, str5));
+		
+		regex1 = "[a-zA-Z]";
+		str1 = "a";
+		str2 = "b";
+		str3 = "c";
+		str4 = "d";
+		str5 = "A";
+		String str6 = "7";
+		System.out.println(Pattern.matches(regex1, str1));
+		System.out.println(Pattern.matches(regex1, str2));
+		System.out.println(Pattern.matches(regex1, str3));
+		System.out.println(Pattern.matches(regex1, str4));
+		System.out.println(Pattern.matches(regex1, str5));
+		System.out.println(Pattern.matches(regex1, str6));
+		
+	}
+	
+}
