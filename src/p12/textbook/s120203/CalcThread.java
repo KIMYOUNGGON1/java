56817b21a2d3824b2cf2381d4f9b76a325675a6a
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-23 11:52:59 +0900

    20210423 3교시 수업내용

---
+++ src/p12/textbook/s120203/CalcThread.java
@@ -0,0 +1,15 @@
+package p12.textbook.s120203;
+
+public class CalcThread extends Thread{
+	public CalcThread(String name) {
+		setName(name);
+	}
+	
+	@Override
+	public void run() {
+		for(int i = 0; i < 2_0000_0000; i++) {
+
+		}
+		System.out.println(getName());
+	}
+}
