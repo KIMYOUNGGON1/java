9b7e57c16bda732251ee7bddc27e056900a3f109
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-28 10:54:41 +0900

    20210428 2교시 수업내용

--- src/p15/lecture/sample/Ex05List.java
+++ src/p15/lecture/sample/Ex05List.java
@@ -1,6 +1,5 @@
 package p15.lecture.sample;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,11 +7,11 @@ import java.util.List;
 public class Ex05List {
 	public static void main(String[] args) {
 		List<Integer> list1 = Arrays.asList(4,6,9,10);
-		List<Integer> list2 = Arrays.asList(-3, -8, -10, -99, -22);
+		List<Integer> list2 = Arrays.asList(-3,-8,-10,-99,-22);
 		List<Integer> list3 = Arrays.asList(8888, 9999, 222, 111);
 		List<Integer> list4 = Arrays.asList(3030,223,123);
 		
-		//List<List<Integer>> list = new ArrayList<List<Integer>>();
+//		List<List<Integer>> list = new ArrayList<List<Integer>>();
 		List<List<Integer>> list = new ArrayList<>();
 		
 		list.add(list1);
@@ -25,14 +24,50 @@ public class Ex05List {
 		
 		max = max2(list);
 		System.out.println(max);
+		
+		List<Integer> flatList = flat(list);
+		System.out.println(flatList.size()); // 16
+		System.out.println(flatList.get(15)); // 123
+		
+		flatList = flat2(list);
+		System.out.println(flatList.size()); // 16
+		System.out.println(flatList.get(15)); // 123
+	}
+
+	private static List<Integer> flat2(List<List<Integer>> list) {
+		List<Integer> flat = new ArrayList<>();
+		
+		for (int i = 0; i < list.size(); i++) {
+			List<Integer> sub = list.get(i);
+			
+			for (int j = 0; j < sub.size(); j++) {
+//				Integer item = sub.get(j);
+//				flat.add(item);
+				flat.add(sub.get(j));
+			}
+		}
+		
+		return flat;
+	}
+
+	private static List<Integer> flat(List<List<Integer>> list) {
+		List<Integer> flat = new ArrayList<>();
+		
+		for (List<Integer> sub : list) {
+			for (Integer item : sub) {
+				flat.add(item);
+			}
+		}
+		
+		return flat;
 	}
 
 	private static int max2(List<List<Integer>> list) {
 		int max = Integer.MIN_VALUE;
 		
-		for(List<Integer> sub : list) {
-			for(int item : sub) {
-				if(item > max) {
+		for (List<Integer> sub : list) {
+			for (int item : sub) {
+				if (item > max) {
 					max = item;
 				}
 			}
@@ -43,11 +78,12 @@ public class Ex05List {
 
 	private static int max(List<List<Integer>> list) {
 		int max = Integer.MIN_VALUE;
-		
+
 		for (int i = 0; i < list.size(); i++) {
-			List<Integer> n = list.get(i);
-			for (int j = 0; j < n.size(); j++) {
-				int item = n.get(j);
+			List<Integer> subList = list.get(i);
+			
+			for (int j = 0; j < subList.size(); j++) {
+				int item = subList.get(j);
 				
 				if (item > max) {
 					max = item;
@@ -58,3 +94,19 @@ public class Ex05List {
 		return max;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/p15/textbook/Board.java
@@ -0,0 +1,13 @@
+package p15.textbook;
+
+public class Board {
+	String subject;
+	String content;
+	String Writer;
+	
+	public Board (String subject, String content, String writer) {
+		this.subject = subject;
+		this.content = content;
+		this.Writer = writer;
+	}
+}
