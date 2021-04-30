7e611b8df23ddece120c1cafb5b57641e60f41e7
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-30 11:01:51 +0900

    20210430 2교시 수업내용

---
+++ src/p15/textbook/exercise/ex09/MapExample.java
@@ -0,0 +1,63 @@
+package p15.textbook.exercise.ex09;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+import java.util.Set;
+
+public class MapExample {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+
+		System.out.println("평균점수: " + avg(map));
+		System.out.println("최고점수: " + maxValue(map));
+		System.out.println("최고점수를 받은 아이디: " + bestId(map));
+	}
+
+	private static String bestId(Map<String, Integer> map) {
+		Set<Map.Entry<String, Integer>> entrise = map.entrySet();
+		int a = Integer.MIN_VALUE;
+		String b = "";
+		for (Map.Entry<String, Integer> entry : entrise) {
+			if (entry.getValue() > a) {
+				a += entry.getValue();
+				if(entrise.equals(a)) {
+					b = entry.getKey();
+				}
+			}
+		}
+		return b;
+	}
+
+	private static int maxValue(Map<String, Integer> map) {
+		Set<Map.Entry<String, Integer>> entrise = map.entrySet();
+		int a = Integer.MIN_VALUE;
+		for (Map.Entry<String, Integer> entry : entrise) {
+			if (entry.getValue() > a) {
+				a = entry.getValue();
+			}
+		}
+		return a;
+	}
+
+	private static int avg(Map<String, Integer> map) {
+		Set<Map.Entry<String, Integer>> entrise = map.entrySet();
+		int a = 0;
+		for (Map.Entry<String, Integer> entry : entrise) {
+			a += entry.getValue();
+		}
+		a /= entrise.size();
+
+		return a;
+	}
+}
