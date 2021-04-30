d91e363ce3a8d6185b592b7a53d0dd691803fe8a
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-04-30 12:36:58 +0900

    20210430 4교시 수업내용

--- src/p15/textbook/exercise/ex09/MapExample.java
+++ src/p15/textbook/exercise/ex09/MapExample.java
@@ -18,12 +18,30 @@ public class MapExample {
 		String name = null;
 		int maxScore = 0;
 		int totalScore = 0;
+		
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			String key = entry.getKey();
+			int value = entry.getValue();
+			
+			if (value > maxScore) {
+				name = key;
+				maxScore = value;
+			}
+			
+			totalScore += value;
+		}
+		
+		System.out.println("평균점수: " + ((double)totalScore / map.size()));
+		System.out.println("최고점수: " + maxScore);
+		System.out.println("최고점수를 받은 아이디: " + name);
 
+
+		/*
 		System.out.println("평균점수: " + avg(map));
 		System.out.println("최고점수: " + maxValue(map));
-		System.out.println("최고점수를 받은 아이디: " + bestId(map));
+		System.out.println("최고점수를 받은 아이디: " + bestId(map)); */
 	}
-
+/*
 	private static String bestId(Map<String, Integer> map) {
 		Set<Map.Entry<String, Integer>> entrise = map.entrySet();
 		int a = Integer.MIN_VALUE;
@@ -60,4 +78,5 @@ public class MapExample {
 
 		return a;
 	}
+	*/
 }
