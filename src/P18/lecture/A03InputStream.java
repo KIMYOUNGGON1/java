ce836e61df989467f185ddb7d4a2ef3551a91212
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-03 10:56:56 +0900

    20210503 2교시 수업내용

--- src/P18/lecture/A03InputStream.java
+++ src/P18/lecture/A03InputStream.java
@@ -9,16 +9,20 @@ public class A03InputStream {
 		
 		InputStream is = new FileInputStream(fileName);
 		
-		byte[] datas = new byte[10];
+		byte[] datas = new byte[20];
 		
-		int cnt = is.read(datas);
+		//int cnt = is.read(datas);
+//		
+//		System.out.println(datas[0]);
+//		System.out.println(datas[1]);
+//		System.out.println(datas[2]);
+//		
+//		System.out.println(cnt);
 		
-		System.out.println(datas[0]);
-		System.out.println(datas[1]);
-		System.out.println(datas[2]);
-		
-		System.out.println(cnt);
-		
+		int cnt = 0;
+		while((cnt = is.read(datas)) != -1) {
+			System.out.println(cnt);
+		}
 		
 		is.close();
 		
