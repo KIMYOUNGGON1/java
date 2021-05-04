f22122d7a328f9279c62283ae2cb414b836e8d9c
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-04 14:59:34 +0900

    20210504 5교시 수업내용

---
+++ src/P18/lecture/network/A01Network.java
@@ -0,0 +1,12 @@
+package P18.lecture.network;
+
+public class A01Network {
+	// internet protocal address (ip address)
+	// wind + r, cmd
+	// ip 주소를 알면 서로 접속이 가능하다.
+	
+	// port number 0 ~ 65535 (0 ~ 1023 x)
+	// socket을 만들어야 함.
+	
+	
+}
