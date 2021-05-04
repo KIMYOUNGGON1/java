389c0168caf0ae113024ab45014e353bd10559ec
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-05-04 10:42:25 +0900

    20210504 2교시 수업내용

---
+++ src/P18/lecture/A21BufferedOutputStream.java
@@ -0,0 +1,22 @@
+package P18.lecture;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+
+public class A21BufferedOutputStream {
+	public static void main(String[] args) throws Exception{
+		String path = "src/P18/lecture/output9.txt";
+		FileOutputStream os = new FileOutputStream(path);
+		BufferedOutputStream bos = new BufferedOutputStream(os);
+		
+		long start = System.nanoTime();
+		for(int i = 0; i<3000; i++) {
+			os.write('a');			
+		}
+		long end = System.nanoTime();
+		System.out.println(end-start);
+		
+		bos.close();
+		os.close();
+	}
+}
